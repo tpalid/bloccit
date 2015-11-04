51277b2b255a3e2f543fc419f94be8181326ed54
Theresa Palid <tpalid1@gmail.com>
2015-11-04 18:43:51 +0000

    Progress on assignment 35

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,42 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params.require(:question).permit(:title, :body))
+      if @question.save
+        flash[:notice] = "Question was saved."
+        redirect_to @question
+      else
+        flash[:error] = "There was an error saving the question.  Please try again."
+        render :new
+      end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(params.require(:question).permit(:title, :body))
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash[:error] = "There was an error updating the question.  Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+end
