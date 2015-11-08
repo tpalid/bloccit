ee64faf60a1b55bcdb5f18e85aeb43586e45df3c
Theresa Palid <tpalid1@gmail.com>
2015-11-08 23:47:44 +0000

    Assignment 38 Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,11 @@ class Post < ActiveRecord::Base
     has_many :comments
     belongs_to :user
     
+    scope :ordered_by_title, -> { order('title ASC') }
+    
+    scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+    
     default_scope { order('created_at DESC') }
 end
+    
+    
