93cbce142846c5e52e72a49c467a06b09976945e
Theresa Palid <tpalid1@gmail.com>
2015-11-08 19:20:05 +0000

    Assignment 37 progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :flash_attack
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
@@ -9,4 +12,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
+  
+  def flash_attack
+    flash[:notice] = "FLASH!"
+  end
+  
 end
