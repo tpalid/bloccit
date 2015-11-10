bc9a0ab280b35c53542719c4ae21f7256ee96c1c
Theresa Palid <tpalid1@gmail.com>
2015-11-10 14:35:19 +0000

    Progress on Assignment-39

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,2 +1,18 @@
 class PostPolicy < ApplicationPolicy
+    class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+        def resolve
+            if user.admin? || user.moderator?
+                scope.all
+            else
+                scope.where(Post.find(:user) == user)
+            end
+        end
+    end
 end
