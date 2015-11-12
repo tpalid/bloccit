b86ce6c001512d6d6b43d97ca708c88fa1d1e67a
Theresa Palid <tpalid1@gmail.com>
2015-11-12 16:29:07 +0000

    Assignment 40 progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110180755) do
+ActiveRecord::Schema.define(version: 20151111182932) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 20151110180755) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "summaries", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "post_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "summaries", ["post_id"], name: "index_summaries_on_post_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
