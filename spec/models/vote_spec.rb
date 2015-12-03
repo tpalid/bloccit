eeb353bc741c47586bae6776680e617a05923e36
Theresa Palid <tpalid1@gmail.com>
2015-12-03 00:16:00 +0000

    checkpoint 49 progress

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Vote do
+    describe "validations" do
+        describe "value validation" do
+            it "only allows -1 or 1 as values" do
+                expect(Vote.create).to eq(1).or eq(-1)
+            end
+        end
+    end
 end
