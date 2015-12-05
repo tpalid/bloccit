347610847ae02bcea3170972c9a895f86c01fd5e
Theresa Palid <tpalid1@gmail.com>
2015-12-05 20:52:44 +0000

    Assignment 49 Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,6 +25,8 @@ class Post < ActiveRecord::Base
     # validates :topic, presence: true
     # validates :user, presence: true
     
+    after_create :create_vote
+    
     def update_rank
         age_in_days = (created_at - Time.new(1970,1,1))/(60*60*24)
         new_rank = points + age_in_days
@@ -32,4 +34,9 @@ class Post < ActiveRecord::Base
         update_attribute(:rank, new_rank)
     end
     
+    private
+    
+    def create_vote
+        user.votes.create(post: self, value: 1)
+    end
 end
