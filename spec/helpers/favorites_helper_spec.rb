f3ae1b5a4decd441e3ac8081f3782f310d6d9265
Theresa Palid <tpalid1@gmail.com>
2015-12-08 03:02:20 +0000

    Assignment 51 progress

--- spec/helpers/favorites_helper_spec.rb
+++ spec/helpers/favorites_helper_spec.rb
@@ -1,15 +1,37 @@
-require 'rails_helper'
+ require 'rails_helper'
+ 
+ describe Comment do
+ 
+   include TestFactories
+ 
+   describe "after_create" do
+ 
+     before do
+       @post = associated_post
+       @user = authenticated_user(email_favorites: true)
+       @other_user = authenticated_user
+       @comment = Comment.new(body: 'My comment is really great', post: @post, user: @other_user)
+     end
+ 
+     it "sends an email to users who have favorited the post" do
+       favorite = @user.favorites.create(post: @post)
+ 
+       allow( FavoriteMailer )
+         .to receive(:new_comment)
+         .with(@user, @post, @comment)
+         .and_return( double(deliver_now: true) )
 
-# Specs in this file have access to a helper object that includes
-# the FavoritesHelper. For example:
-#
-# describe FavoritesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-RSpec.describe FavoritesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+         expect( FavoriteMailer )
+           .to receive(:new_comment)
+ 
+       @comment.save
+     end
+ 
+     it "does not send emails to users who haven't" do
+       expect( FavoriteMailer )
+         .not_to receive(:new_comment)
+ 
+       @comment.save
+     end
+   end
+ end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,20 @@ describe User do
 
     include TestFactories
     
+    
     describe "#favorited(post)" do
-        xit "returns 'nil' if the user has not favorited the post" do
+        before do
+            @user = authenticated_user
+            @post1 = associated_post
+            @post2 = associated_post
+            @favorite = Favorite.new(user: @user, post: @post2 )
+         end
+        it "returns 'nil' if the user has not favorited the post" do
+            expect( favorited(@post1).to be_false)
         end
         
-        xit "returns the appropriate favorite if it exists" do
+        it "returns the appropriate favorite if it exists" do
+            expect( favorited(@post2).to be_true)
         end
     end
 end
