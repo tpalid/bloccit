a62d97a6c543c6cbc12c70d102ed69861bf5bc78
Theresa Palid <tpalid1@gmail.com>
2015-12-09 19:06:07 +0000

    Assignment 52 progress

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,18 @@ class Topic < ActiveRecord::Base
     validates :name, length: { minimum: 5 }, presence: true
     validates :description, presence: true
     
-    scope :visible_to, -> (user) { user ? all : where(public: true) }
+    scope :visible_to, ->(user) { user ? all : publicly_viewable }
     
+    scope :publicly_viewable, -> { where(public: true) }
+    
+    scope :privately_viewable, -> { where(public: false) }
+    
+    #also tried defining class methods, with the same error result
+    # def self.publicly_viewable
+    #     where(public: true)
+    # end
+    
+    # def self.privately_viewable
+    #     where(public: false)
+    # end
 end
