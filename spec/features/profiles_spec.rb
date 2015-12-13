c12733737cb8220385c8b53ab907cc1a85253d89
Theresa Palid <tpalid1@gmail.com>
2015-12-13 19:01:48 +0000

    Assignment 55 progress

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
  describe "Visiting profiles" do
  
    include TestFactories
- 
+   include Warden::Test::Helpers
+   Warden.test_mode!
+   
    before do
      @user = authenticated_user
      @post = associated_post(user: @user)
@@ -22,6 +24,21 @@ require 'rails_helper'
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
      end
- 
+    end
+     
+    describe "signed_in" do
+     
+     before do
+      login_as(@user, :scope => :user)
+     end
+      
+     it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+      
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+     end
    end
  end
